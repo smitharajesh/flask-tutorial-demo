e3b37b04285ce738ecf698fb80bd258e27fee940
smitha <smitha@qxf2.com>
2018-11-27 14:59:21 +0530

    modified py file and added templates dir

--- mybio.py
+++ mybio.py
@@ -1,16 +1,18 @@
 """
 Flask app to host my simple bio
 Habit: Develop -> test -> commit -> deploy -> test
+
 """
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index_page():
     "The search page"
-    return "Hello"
+    return render_template('index.html')
 
 #----START OF SCRIPT
 if __name__=='__main__':
